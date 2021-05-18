95af5e65b6e9fcc3a9e731a53f50f64bab6c6807
nipan09 <pandey.nishant489@gmail.com>
2021-05-18 17:39:59 +0530

    file import fixed

--- rtc/chat/consumers.py
+++ rtc/chat/consumers.py
@@ -1,9 +1,15 @@
 import json
-from channels.generic.websocketConsumer
+from channels.generic.websocket import WebsocketConsumer
 
-class ChatRoomConsumer(websocketConusmer):
-    def connect(self):
-        pass
+class ChatRoomConsumer(WebsocketConsumer):
+    async def connect(self):
+        self.room_name = self.scope['url_router']['kwargs']['room_name']
+        self.room_group_name = 'chat_%s' % self.room_name
 
+        await self.channel_layer.group_add(
+            self.room_group_name
+            self.channel_name
+        )
+        
     def disconnect(self, close_code):
         pass
