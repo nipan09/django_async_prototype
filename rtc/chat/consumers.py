04e4dd22d909d3d26802f91437d13707c8a2281b
nipan09 <pandey.nishant489@gmail.com>
2021-05-18 17:11:49 +0530

    consumer init and routing added

---
+++ rtc/chat/consumers.py
@@ -0,0 +1,9 @@
+import json
+from channels.generic.websocketConsumer
+
+class ChatRoomConsumer(websocketConusmer):
+    def connect(self):
+        pass
+
+    def disconnect(self, close_code):
+        pass
