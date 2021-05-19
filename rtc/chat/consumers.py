243e5fce054da6937c4756014f00e8dd63a534df
nipan09 <pandey.nishant489@gmail.com>
2021-05-19 19:44:36 +0530

    disconnect and recieve methods added

--- rtc/chat/consumers.py
+++ rtc/chat/consumers.py
@@ -2,14 +2,30 @@ import json
 from channels.generic.websocket import WebsocketConsumer
 
 class ChatRoomConsumer(WebsocketConsumer):
+    
     async def connect(self):
         self.room_name = self.scope['url_router']['kwargs']['room_name']
         self.room_group_name = 'chat_%s' % self.room_name
 
         await self.channel_layer.group_add(
-            self.room_group_name
+            self.room_group_name,
             self.channel_name
         )
+
+        await self.accept()
+
+    async def disconnect(self, close_code):
         
-    def disconnect(self, close_code):
-        pass
+        await self.channel_layer.group_discard(
+            self.room_group_name,
+            self.channel_name
+        )
+
+    # Recieve message from WebSocket
+    async def receive(self, text_data):
+        text_data_json = json.loads(text_data)
+        message = text_data_json['message']
+
+        self.send(text_data=json.dumps(
+            {'message': message}
+        ))
