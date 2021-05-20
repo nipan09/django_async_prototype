7c53db8fad0f86eba6d18a9b951623ba6d037f82
nipan09 <pandey.nishant489@gmail.com>
2021-05-20 20:53:16 +0530

    recieving message and send over websocket implemented

--- rtc/chat/consumers.py
+++ rtc/chat/consumers.py
@@ -16,23 +16,8 @@ class ChatRoomConsumer(AsyncWebsocketConsumer):
         await self.accept()
         print('Connected!!')
 
-        await self.channel_layer.group_send(
-            self.room_group_name,
-            {
-                'type': 'tester_message',
-                'tester' : 'hello world',
-            }
-        )
-
-    async def tester_message(self, event):
-        tester = event['tester']
-
-        await self.send(text_data=json.dumps({
-            'tester' : tester,
-        }))
-
     async def disconnect(self, close_code):
-        
+
         await self.channel_layer.group_discard(
             self.room_group_name,
             self.channel_name
@@ -45,8 +30,42 @@ class ChatRoomConsumer(AsyncWebsocketConsumer):
         username = text_data_json['username']
 
         print(message)
-        print(username)
 
-        self.send(text_data=json.dumps(
-            {'message': message}
-        ))
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': 'chat_message',
+                'message': message,
+                'username': username,
+            }
+        )
+    async def chat_message(self, event):
+        message = event['message']
+        username = event['username']
+
+        await self.send(text_data=json.dumps({
+            'message': message,
+            'username': username,
+        }))
+
+
+
+        #Below is just a prototype of how to send the data over socket.
+        #It was be used to send the captured data later in 'recieve' function
+'''        
+        # function will send the event over the channel layer to the group
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': 'tester_message',
+                'tester' : 'hello world',
+            }
+        )
+
+    # function handling the above event to recieve those events and turn them into Web Socket frames
+    async def tester_message(self, event):
+        tester = event['tester']
+        await self.send(text_data=json.dumps({
+            'tester' : tester,
+        }))
+'''
